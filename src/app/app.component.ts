1c7f3970a0840ff4859cf1fec14d511c3ef90bb9
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2018-05-09 11:57:08 +0200

    code reorganized / version check + network status check added to core module / material components added to sharedmodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { CheckNetworkStatusService } from './core/check-network-status.service';
+import { CheckForUpdateService } from './core/check-for-update.service';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { interval } from 'rxjs/observable/interval';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  public title = 'app';
+
+  constructor(
+    private checkForUpdateService: CheckForUpdateService,
+    private checkNetworkStatusService: CheckNetworkStatusService) { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatButtonModule, MatCheckboxModule, MatCardModule, MatSelectModule, MatToolbarModule  } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -18,14 +14,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    NgbModule.forRoot(),
-    MatButtonModule,
-    MatCheckboxModule,
-    MatCardModule,
-    MatSelectModule,
-    MatToolbarModule,
     CoreModule,
-    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
