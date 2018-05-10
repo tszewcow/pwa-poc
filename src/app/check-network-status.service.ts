a59b749adb6c3237c6dac4f9680de2afafdf608d
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2018-05-10 15:44:08 +0200

    network status check run outside angular zone

--- src/app/check-network-status.service.ts
+++ src/app/check-network-status.service.ts
@@ -1,22 +1,23 @@
-import {Injectable} from '@angular/core';
-import {interval} from 'rxjs/observable/interval';
-import {MatSnackBar} from '@angular/material';
+import { Injectable, NgZone } from '@angular/core';
+import { interval } from 'rxjs/observable/interval';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class CheckNetworkStatusService {
 
   private snackBarRef;
 
-  constructor(private snackBar: MatSnackBar) {
+  constructor(private snackBar: MatSnackBar, private ngZone: NgZone) {
     console.log('Registering CheckNetworkStatusService');
-    interval(2000).subscribe(() => {
-      if (!navigator.onLine && !this.snackBarRef) {
-        this.snackBarRef = this.snackBar.open('Offline mode, please reconnect the device in order to use all app features.');
-      } else if (navigator.onLine && this.snackBarRef) {
-        this.snackBarRef.dismiss();
-        this.snackBarRef = undefined;
-      }
+    this.ngZone.runOutsideAngular(() => {
+      interval(2000).subscribe(() => {
+        if (!navigator.onLine && !this.snackBarRef) {
+          this.snackBarRef = this.snackBar.open('Offline mode, please reconnect the device in order to use all app features.');
+        } else if (navigator.onLine && this.snackBarRef) {
+          this.snackBarRef.dismiss();
+          this.snackBarRef = undefined;
+        }
+      });
     });
   }
-
 }
