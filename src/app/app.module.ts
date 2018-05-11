6d30e3c0b7ead0560d4de17352cd7970c65d4a5a
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2018-05-11 13:01:46 +0200

    initial home route for gh-pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppComponent} from './app.component';
-import {CoreModule} from './core/core.module';
-import {SharedModule} from './shared/shared.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
-import {CheckNetworkStatusService} from './check-network-status.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { CheckNetworkStatusService } from './check-network-status.service';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/#home', pathMatch: 'full' },
+  { path: '#home', component: AppComponent }
+];
 
 
 @NgModule({
@@ -18,6 +24,7 @@ import {CheckNetworkStatusService} from './check-network-status.service';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
     CoreModule,
     SharedModule,
     environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
